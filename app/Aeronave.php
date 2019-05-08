67b5402909b88818eb6899ccb34d021af31c48fa
Rafael Correia <rafa_t_correia@hotmail.com>
2019-05-08 23:29:37 +0100

    Alterações nos Modelos

--- app/Aeronave.php
+++ app/Aeronave.php
@@ -16,8 +16,7 @@ class Aeronave extends Model
     public $incrementing = false;
 
     protected $fillable = [
-        'matricula', 'marca', 'modelo','num_lugares',
-        'conta_horas', 'preco_hora'
+        'matricula', 'marca', 'modelo'
     ];
 
 
