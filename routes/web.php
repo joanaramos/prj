b5f26011acaa56db120094f98b15d24a8cc08e6d
Rafael Correia <rafa_t_correia@hotmail.com>
2019-05-09 00:11:52 +0100

    Adição rotas aeronaves

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+//Rotas para user
 Route::get('users', 'UserController@index')->name('users.index');
 Route::get('create', 'UserController@create')->name('users.create');
 Route::post('users', 'UserController@store')->name('users.store');
 Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit');
 Route::put('users/{user}', 'UserController@update')->name('users.update');
 Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+
+
+//--------------------------------------------------------------------------------
+//Rotas para Aeronaves
+Route::get('aeronaves', 'AeronaveController@index')->name('aeronaves.index');
+Route::get('aeronaves/create', 'AeronaveController@create')->name('aeronaves.create');
+Route::post('aeronaves', 'AeronaveController@store')->name('aeronaves.store');
+Route::get('aeronaves/{aeronave}/edit', 'AeronaveController@edit')->name('aeronaves.edit');
+Route::put('aeronaves/{aeronave}', 'AeronaveController@update')->name('aeronaves.update');
+Route::delete('aeronaves/{aeronave}', 'AeronaveController@destroy')->name('aeronaves.destroy');
+
+//-----------------------------------------------------------------------------------
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Auth::routes();
