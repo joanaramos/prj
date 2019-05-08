5f85967bd04bc26c72a31c31053e04176d737e92
Rafael Correia <rafa_t_correia@hotmail.com>
2019-05-08 23:22:50 +0100

    Criação Modelo "Aeronave" e Controlador "AeronaveControler"

--- app/Aeronave.php
+++ app/Aeronave.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aeronave extends Model
 {
-    //
+    // Overrides table name
+    protected $table = 'aeronaves';
+
+    // Overrides primary key
+    protected $primaryKey = 'matricula';
+
+    // Disables auto increment primary key
+    public $incrementing = false;
+
+    protected $fillable = [
+        'matricula', 'marca', 'modelo','num_lugares',
+        'conta_horas', 'preco_hora'
+    ];
+
+
 }
